39230336cde24f183ff459f57bb2e6b09a609460
Mustafa Senses <mustafasenses@delivery-one.com>
2024-08-29 01:10:59 +0300

    fix

--- app/communication/iot/mqtt_message_event.py
+++
@@ -1,9 +0,0 @@
-class MqttMessageEvent:
-    sample_time: object = None
-    cmd: int = None
-    data: object = None
-
-    def __init__(self, sample_time: object, cmd: int, data: object):
-        self.sample_time = sample_time
-        self.cmd = cmd
-        self.data = data
