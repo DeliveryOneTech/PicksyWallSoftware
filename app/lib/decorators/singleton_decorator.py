46500f11265f94ffb6e7366c2a07f456ababe380
Mustafa Senses <mustafasenses@delivery-one.com>
2024-09-10 01:33:02 +0300

    comments

--- app/lib/decorators/singleton_decorator.py
+++ app/lib/decorators/singleton_decorator.py
@@ -6,11 +6,14 @@ def Singleton(cls):
     if not isinstance(cls, type):
         raise TypeError("Singleton decorator must be applied to a class.")
 
-    instances = {}
+    instances = {}  # Bu sözlük, her sınıf için bir örneğin saklanmasını sağlar.
 
+    # get_instance fonksiyonu, sınıfın bir örneğini oluştururken, o sınıfın daha önce oluşturulup oluşturulmadığını kontrol eder.
     def get_instance(*args, **kwargs):
+        # Eğer sınıfın örneği daha önce oluşturulmamışsa, yeni bir örnek oluşturur.
         if cls not in instances:
             instances[cls] = cls(*args, **kwargs)
+        # Eğer sınıfın örneği daha önce oluşturulmuşsa, o örneği döndürür.
         return instances[cls]
 
     return get_instance
