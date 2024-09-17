8e8a25fa254caa816bd54dc97ba9ad30730798fa
Mustafa Senses <mustafasenses@delivery-one.com>
2024-09-17 04:52:01 +0300

    visual works

--- app/ui/pages/robot_management_main_page.py
+++ app/ui/pages/robot_management_main_page.py
@@ -13,41 +13,39 @@ class RobotManagementMainPage(QWidget):
         self.caller_page_number = caller_page_number
 
         self.setStyleSheet("QPushButton {font: 16pt 'MS Shell Dlg 2';}")
-        v_box = QVBoxLayout()
+        grid_layout = QGridLayout()
 
         label = 'Sol' if caller_page_number == PageNumber.LEFT_ROBOT_MANAGEMENT else 'Sağ'
 
-        header = ServiceMainHeaderComponent(label)
-        footer = None
+        header = ServiceMainHeaderComponent(label + ' Robot')
 
         if header:
             header.back_button_clicked.connect(lambda: self.stacked_widget.go_by_page_number(
                 self.caller_page_number, PageNumber.SERVICE_MAIN
             ))
-            v_box.addWidget(header)
+            grid_layout.addWidget(header, 0, 0, 1, 3)
 
         '''
         begin - content
         '''
-        v_box.addSpacing(50)
 
         self.connection_check_button = QPushButton('Bağlantı Kontrolü')
-        self.connection_check_button.setStyleSheet(Styles.btn_success(padding="25px 40px"))
+        self.connection_check_button.setStyleSheet(Styles.btn_success(padding="10px"))
 
         self.robot_check_button = QPushButton('Senaryo Kontrolü')
-        self.robot_check_button.setStyleSheet(Styles.btn_success(padding="25px 40px"))
+        self.robot_check_button.setStyleSheet(Styles.btn_success(padding="10px"))
 
         self.lighting_check_button = QPushButton('Aydınlatma Kontrolü')
-        self.lighting_check_button.setStyleSheet(Styles.btn_success(padding="25px 40px"))
+        self.lighting_check_button.setStyleSheet(Styles.btn_success(padding="10px"))
 
         self.relay_check_button = QPushButton('Röle Kontrolü')
-        self.relay_check_button.setStyleSheet(Styles.btn_success(padding="25px 40px"))
+        self.relay_check_button.setStyleSheet(Styles.btn_success(padding="10px"))
 
         self.package_measuring_system_button = QPushButton('Paket Ölçüm Sistemi')
-        self.package_measuring_system_button.setStyleSheet(Styles.btn_success(padding="25px 40px"))
+        self.package_measuring_system_button.setStyleSheet(Styles.btn_success(padding="10px"))
 
         self.restart_button = QPushButton('Cihazı Yeniden Başlat')
-        self.restart_button.setStyleSheet(Styles.btn_danger(padding="25px 40px"))
+        self.restart_button.setStyleSheet(Styles.btn_danger(padding="10px"))
 
         self.x1_axis = DeltaNumericInputComponent('X1 Ekseni')
 
@@ -61,32 +59,20 @@ class RobotManagementMainPage(QWidget):
 
         self.hide_cover = DeltaNumericInputComponent('Gizleme Kapağı')
 
-        grid_layout = QGridLayout()
-        grid_layout.addWidget(self.connection_check_button, 0, 0)
-        grid_layout.addWidget(self.robot_check_button, 0, 1)
-        grid_layout.addWidget(self.lighting_check_button, 0, 2)
-        grid_layout.addWidget(self.relay_check_button, 1, 0)
-        grid_layout.addWidget(self.package_measuring_system_button, 1, 1)
-        grid_layout.addWidget(self.restart_button, 1, 2)
-        grid_layout.addWidget(self.x1_axis, 2, 0)
-        grid_layout.addWidget(self.z_axis, 2, 1)
-        grid_layout.addWidget(self.delivery_cover, 2, 2)
-        grid_layout.addWidget(self.x2_axis, 3, 0)
-        grid_layout.addWidget(self.y_axis, 3, 1)
-        grid_layout.addWidget(self.hide_cover, 3, 2)
-
-        grid_layout.setHorizontalSpacing(75)
-        grid_layout.setVerticalSpacing(45)
-
-        v_box.addLayout(grid_layout)
-
-        v_box.addStretch()
+        grid_layout.addWidget(self.connection_check_button, 1, 0)
+        grid_layout.addWidget(self.robot_check_button, 1, 1)
+        grid_layout.addWidget(self.lighting_check_button, 1, 2)
+        grid_layout.addWidget(self.relay_check_button, 2, 0)
+        grid_layout.addWidget(self.package_measuring_system_button, 2, 1)
+        grid_layout.addWidget(self.restart_button, 2, 2)
+        grid_layout.addWidget(self.x1_axis, 3, 0)
+        grid_layout.addWidget(self.z_axis, 3, 1)
+        grid_layout.addWidget(self.delivery_cover, 3, 2)
+        grid_layout.addWidget(self.x2_axis, 4, 0)
+        grid_layout.addWidget(self.y_axis, 4, 1)
+        grid_layout.addWidget(self.hide_cover, 4, 2)
 
         '''
         end - content
         '''
-
-        if footer:
-            v_box.addWidget(footer)
-
-        self.setLayout(v_box)
+        self.setLayout(grid_layout)
