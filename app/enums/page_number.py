3ee97a362eb2430eac4753578c2e3e0d690ecbc3
Mustafa Senses <mustafasenses@delivery-one.com>
2024-10-15 02:14:25 +0300

    enum file number order fix

--- app/enums/page_number.py
+++ app/enums/page_number.py
@@ -3,25 +3,17 @@ from enum import Enum
 
 class PageNumber(Enum):
     HOME_PAGE = 0
-
-    SENDER_IDENTIFICATION_PAGE = 1  # sender identification page
-    SENDER_INFORMATION_PAGE = 11  # sender information page
-    SENDER_ADDRESS_PAGE = 12  # sender address page
-    RECEIVER_INFORMATION_PAGE = 13  # receiver information page
-    RECEIVER_ADDRESS_PAGE = 14  # receiver address page
-
-    REJECTION_ID_PAGE = 2  # rejection id page
-
-    INITIALIZATION_PAGE = 5  # initialaztion page
-    RECEIVER_AUTHENTICATION_PAGE = 3  # receiver authentication page
-
-    OTHER_SERVICES_PAGE = 6  # other services page
-    COURIER_AUTHENTICATION_PAGE = 7  # courier authentication page
-
+    SENDER_IDENTIFICATION_PAGE = 1
+    REJECTION_ID_PAGE = 2
+    RECEIVER_AUTHENTICATION_PAGE = 3
     SERVICE_USER_AUTHENTICATION_PAGE = 4
-    SERVICE_MAIN_PAGE = 8  # service main page
-    LEFT_ROBOT_MANAGEMENT_PAGE = 9  # left robot management page
-    RIGHT_ROBOT_MANAGEMENT_PAGE = 10  # right robot management page
-
-    # MAINTENANCE =
-    # CLEANING =
+    INITIALIZATION_PAGE = 5
+    OTHER_SERVICES_PAGE = 6
+    COURIER_AUTHENTICATION_PAGE = 7
+    SERVICE_MAIN_PAGE = 8
+    LEFT_ROBOT_MANAGEMENT_PAGE = 9
+    RIGHT_ROBOT_MANAGEMENT_PAGE = 10
+    SENDER_INFORMATION_PAGE = 11
+    SENDER_ADDRESS_PAGE = 12
+    RECEIVER_INFORMATION_PAGE = 13
+    RECEIVER_ADDRESS_PAGE = 14
