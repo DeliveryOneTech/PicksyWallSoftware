c87d44df800b82a37024f1df38930516ede9262b
Mustafa Senses <mustafasenses@delivery-one.com>
2024-10-19 01:44:33 +0300

    Application mode field added to appconfig.json

--- app/init.py
+++ app/init.py
@@ -1,8 +1,8 @@
-import logging
 from PyQt5.QtWidgets import QApplication
 import sys
 from app.communication.iot.mqtt_subscriber import MqttSubscriber
 from app.communication.iot.mqtt_context import MqttContext
+from app.lib.utils import Utils
 from app.ui.main_window import MainWindow
 from app.lib.console_logger import ConsoleLogger
 from app.services.log_service import LogService
@@ -22,9 +22,14 @@ def run() -> int:
     Returns:
         int: The exit status code.
     """
+    application_mode = Utils.get_value_from_app_config('ApplicationMode')
     app = QApplication(sys.argv)
     window = MainWindow()
-    window.showFullScreen()
+
+    if application_mode == 'development':
+        window.show()
+    else:
+        window.showFullScreen()
 
     app.aboutToQuit.connect(__on_close_app)
     return sys.exit(app.exec())
