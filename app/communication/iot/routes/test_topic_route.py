498fdab86878e1d4844910c233fa426245b6d88a
Mustafa Senses <mustafasenses@delivery-one.com>
2024-10-19 15:25:11 +0300

    fix

--- app/communication/iot/routes/test_topic_route.py
+++ app/communication/iot/routes/test_topic_route.py
@@ -1,7 +1,6 @@
 from app.communication.iot.routes.base_route import MqttMessageRoute
 from app.enums.iot_topic import IoTTopic
 from app.lib.utils.console_logger import ConsoleLogger
-from app.lib.utils.utils import Utils
 
 
 class TestTopicRoute(MqttMessageRoute):
